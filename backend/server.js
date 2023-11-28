392460e7e61444d9f229b16ffa90fa8a29ec4315
hridoy <ridoy1907104@stud.kuet.ac.bd>
2023-11-29 00:04:03 +0600

    fetch product from mongodb

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();  // to use .env file
 import connectDB from './config/db.js';
-import products from './data/products.js';
+
+import productRoutes from './routes/productRoutes.js';
 const port = process.env.PORT || 5000;
 
 connectDB();
@@ -15,12 +16,6 @@ app.get('/', (req, res) => {
     res.send('API is running');
 });
 
-app.get('/api/products', (req, res) => {
-    res.json(products);
-});
+app.use('/api/products', productRoutes);
 
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find((p) => p._id === req.params.id);
-    res.json(product);
-});
 app.listen(port , () => console.log(`server running on port ${port}`));
