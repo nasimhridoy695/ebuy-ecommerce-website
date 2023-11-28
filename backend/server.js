93f6f67998b93a9a8d5b858d34a80546dfab3587
hridoy <ridoy1907104@stud.kuet.ac.bd>
2023-11-29 01:32:37 +0600

    added api Slice, Loader, alert message

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();  // to use .env file
 import connectDB from './config/db.js';
-
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import productRoutes from './routes/productRoutes.js';
 const port = process.env.PORT || 5000;
 
@@ -17,5 +17,8 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/products', productRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port , () => console.log(`server running on port ${port}`));
